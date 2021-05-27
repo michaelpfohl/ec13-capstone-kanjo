d705b6056dc80153d932515816a0f85cb8ebc021
Michael Pfohl <rmpfohl@gmail.com>
2021-05-27 14:42:40 -0500

    routes and navbar complete

--- kanjo.ui/src/Helpers/routes.tsx
+++ kanjo.ui/src/Helpers/routes.tsx
@@ -2,12 +2,17 @@ import { Switch, Route } from 'react-router-dom';
 
 import NotFound from '../Views/NotFound';
 import Home from '../Views/Home';
+import Emotion from '../Views/Emotion';
+import Entry from '../Views/Entry';
+import Examine from '../Views/Examine';
 
 export default function Routes(): JSX.Element {
     return (
         <Switch>
             <Route exact path="/" component={() => <Home />}/>
-            <Route exact path="/new-entry"/>
+            <Route exact path="/emotion" component={() => <Emotion />}/>
+            <Route exact path="/entry" component={() => <Entry />}/>
+            <Route exact path="/examine" component={() => <Examine />}/>
             <Route component={NotFound}/>
         </Switch>
     );  
---
+++ kanjo.ui/src/Views/Emotion.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Emotion(): JSX.Element {
+  return (
+    <div className='d-flex justify-content-center m-5'>
+      <h1>emotion</h1>
+    </div>
+  );
+}
