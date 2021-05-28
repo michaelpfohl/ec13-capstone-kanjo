eeb8781caeb9c05d93ff5d9b51548aa4f1d9c8fb
Michael Pfohl <rmpfohl@gmail.com>
2021-05-28 15:05:16 -0500

    authenticated & non home view

---
+++ kanjo.ui/src/Helpers/Types/PropTypes.d.ts
@@ -0,0 +1,19 @@
+import { User } from './UserTypes';
+
+declare module "PropTypes" {
+  type HomeProps = {
+    user: User | null;
+    loginClickEvent: (
+        e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+      ) => void;
+  };
+
+  type RoutesProps = {
+    user: User | null;
+    loginClickEvent: (
+      e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+    ) => void;
+  };
+}
+
+export { HomeProps, RoutesProps };
--- kanjo.ui/src/Views/Home.tsx
+++ kanjo.ui/src/Views/Home.tsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { Component } from "react";
+import { HomeProps } from "../Helpers/Types/PropTypes";
 
-export default function Home(): JSX.Element {
-  return (
-    <div className='d-flex justify-content-center m-5'>
-      <h1>home</h1>
-    </div>
-  );
+class Home extends Component<HomeProps> {
+  render(): JSX.Element {
+    const { user, loginClickEvent } = this.props;
+    return (
+      <div>
+        {!user && (
+          <div className="d-flex justify-content-center m-5">
+            <div className="text-center welcome-container">
+              <h1>welcome to kanjo</h1>
+              <h3 className="mb-5">
+                a self guided exploration into your daily emotions
+              </h3>
+              <h3 className="mb-3">click the icon below to sign in</h3>
+              <button className="sign-in-circle" onClick={loginClickEvent}>
+                sign in
+              </button>
+            </div>
+          </div>
+        )}
+        {user && (
+          <div className="d-flex justify-content-center m-5">
+            <div className="text-center welcome-container">
+              <h1>welcome to kanjo</h1>
+              <h3 className="mb-5">
+                a self guided exploration into your daily emotions
+              </h3>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
+
+export default Home;
